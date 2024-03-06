42f2a55f6020c055302b00e154d621f76ee23317
sahil sartanpara <138644834+sartanparasahil@users.noreply.github.com>
2024-03-06 12:56:01 +0530

    Delete plan and trainer

--- server/src/features/Plans/plan.control.js
+++ server/src/features/Plans/plan.control.js
@@ -1,17 +1,43 @@
 const planModel = require("./plans.model");
+const { param } = require("./plans.router");
 
-const plan = async ( req , res ) =>{
-    const{ name , image , desc , type , gender , duration , qty , price } = req.body;
-    const namechk = await planModel.findOne({name});
-    if(!(name && image && desc && type && gender && duration && qty && price)){
-        return res.status(400).json({message:"All Fileds Are Required"});
+const plan = async (req, res) => {
+    const { name, image, desc, type, gender, duration, qty, price } = req.body;
+    const namechk = await planModel.findOne({ name });
+    if (!(name && image && desc && type && gender && duration && qty && price)) {
+        return res.status(400).json({ message: "All Fileds Are Required" });
     }
-    if(namechk){
-        return res.status(400).json({message:"PlanName Already Exists"});
+    if (namechk) {
+        return res.status(400).json({ message: "PlanName Already Exists" });
     }
-    const newplan = await new planModel({ name , image , desc , type , gender , duration , qty , price });
+    const newplan = await new planModel({ name, image, desc, type, gender, duration, qty, price });
     newplan.save();
-    return res.status(200).json({message:"Plan Added SuccessFully",data:newplan});
+    return res.status(200).json({ message: "Plan Added SuccessFully", data: newplan });
 }
 
-module.exports = plan;
+const DeletePlan = async (req, res) => {
+
+    try {
+        const id = req.params.id
+        console.log(id);
+
+        const isvalidPlan = await planModel.findById(id)
+        
+        if (isvalidPlan) {
+            await planModel.findByIdAndDelete(id)
+            return res.status(200).json({ message: "Plan Deleted SuccessFully....." });
+        }
+
+        return res.status(200).json({ message: "id not found" });
+
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: "Server failed" })
+    }
+
+
+
+}
+
+module.exports = { plan, DeletePlan };
--- server/src/features/coach/coach.control.js
+++ server/src/features/coach/coach.control.js
@@ -20,4 +20,18 @@ const GetTrainer = async ( req , res ) => {
     return res.status(200).json(Data);
 }
 
-module.exports = { trainer , GetTrainer };
+const DeleteTrainer = async ( req , res ) =>  {
+    const id = req.params.id;
+    try {
+        const isvalidtrainer = await trainermodel.findOne({id});
+        if(isvalidtrainer){
+            await trainermodel.findByIdAndDelete(id);
+            return res.status(200).json({ message: "Trainer Deleted SuccessFully....." });
+        }
+        return res.status(400).json({message:"Id Is Not Found"});
+    } catch (error) {
+        return res.status(500).json({message:"Server Failed!"});        
+    }
+}
+
+module.exports = { trainer , GetTrainer , DeleteTrainer };
